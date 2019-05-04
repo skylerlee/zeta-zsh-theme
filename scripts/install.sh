832eccc714880577a13e75c6a1569332b60da924
skylerlee <skyler.ac.lee@gmail.com>
2019-05-04 23:51:29 +0800

    Update install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,6 +2,27 @@
 
 THEME_ZSH_FILE=https://raw.githubusercontent.com/skylerlee/zeta-zsh-theme/master/zeta.zsh-theme
 
+function init_colors {
+  if which tput >/dev/null 2>&1; then
+    local ncolors=$(tput colors)
+  fi
+  if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
+    RED="$(tput setaf 1)"
+    GREEN="$(tput setaf 2)"
+    YELLOW="$(tput setaf 3)"
+    BLUE="$(tput setaf 4)"
+    BOLD="$(tput bold)"
+    RESET="$(tput sgr0)"
+  else
+    RED=""
+    GREEN=""
+    YELLOW=""
+    BLUE=""
+    BOLD=""
+    RESET=""
+  fi
+}
+
 function fetch_file {
   local file_url="$1"
   local dest_dir="$2"
@@ -14,6 +35,13 @@ function set_theme {
   sed -i.bak -e "s/^ZSH_THEME=[\"']\?[A-Za-z0-9\._-]\+[\"']\?/ZSH_THEME=\"$1\"/1" $HOME/.zshrc
 }
 
+echo "Installing ..."
+
 fetch_file $THEME_ZSH_FILE $ZSH_CUSTOM/themes
 set_theme zeta
+
+printf "$GREEN"
+echo "Welcome to use Zeta theme :)"
+printf "$RESET"
+
 env zsh -l
